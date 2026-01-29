88b5efba13af17659062dcc1ea34063c5ca6d89e
Jumedeen khan <admin@mozedia.com>
2026-01-29 17:08:41 +0530

    Update lazy-load-for-images.php
    Update version 2.0.0

--- lazy-load-for-images.php
+++ lazy-load-for-images.php
@@ -1,64 +1,55 @@
 <?php
-
 /**
- * Plugin Name: Lazy Load for Images
- * Plugin URI:  https://www.mozedia.com/lazy-load-images-in-wordpress/
- * Description: Lazyload wordpress images with a small javascript without jQuery or others libraries.
- * Author:      Jumedeen khan
+ * Plugin Name: Smart LazyLoad
+ * Plugin URI:  https://www.mozedia.com/lazy-load-wordpress/
+ * Description: A lightweight, pure JavaScript solution to lazy load images, iframes, and videos without jQuery or external libraries.
+ * Version: 2.0.0
+ * Author: Jumedeen Khan
  * Author URI:  https://www.mozedia.com/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 5.8
+ * Requires PHP: 7.2
+ * Tested up to: 6.9
  * Text Domain: lazy-load-for-images
- * Version:     1.4.2
- *
  */
 
-//* Add filter to replace gravatar, post content and widget images
-add_filter( 'get_avatar', 'mozedia_lazyload_images', PHP_INT_MAX );
-add_filter( 'the_content', 'mozedia_lazyload_images', PHP_INT_MAX );
-add_filter( 'widget_text', 'mozedia_lazyload_images', PHP_INT_MAX );
-add_filter( 'post_thumbnail_html', 'mozedia_lazyload_images', PHP_INT_MAX );
-
-//* Also support genesis thumbnails (if is available)
-add_filter( 'genesis_get_image', 'mozedia_lazyload_images', PHP_INT_MAX );
-
-//* start function for lazy load images
-function mozedia_lazyload_images( $html ) {
-	// Don't LazyLoad if the thumbnail is in
-	if( is_admin() || is_feed() || is_preview() || empty( $html ) ) {
-		return $html;
-	}
-
-	// Stop LalyLoad process with this hook
-	if ( ! apply_filters( 'do_not_lazyload', true ) ) {
-		return $html;
-	}
-
-	$html = preg_replace_callback( '#<img([^>]*) src=("(?:[^"]+)"|\'(?:[^\']+)\'|(?:[^ >]+))([^>]*)>#', '__mozedia_lazyload_replace_callback', $html );
-
-	return $html;
+/**
+ * Prevent direct access to this file.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
+/**
+ * Plugin core constants.
+ *
+ * These constants are used throughout the plugin
+ * to manage paths, URLs, versioning, and references.
+ */
+define( 'MLL_VERSION', '2.0.0' );
+define( 'MLL_PATH', plugin_dir_path( __FILE__ ) );
+define( 'MLL_URL', plugin_dir_url( __FILE__ ) );
+define( 'MLL_BASENAME', plugin_basename( __FILE__ ) );
 
-//* Used to check if LazyLoad this or not
-function __mozedia_lazyload_replace_callback( $matches ) {
-	if ( strpos( $matches[1] . $matches[3], 'data-no-lazy=' ) === false && strpos( $matches[1] . $matches[3], 'data-src=' ) === false && strpos( $matches[2], '/wpcf7_captcha/' ) === false ) {
-		$html = sprintf( '<img%1$s src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src=%2$s%3$s><noscript><img%1$s src=%2$s%3$s></noscript>',
-						$matches[1], $matches[2], $matches[3] );
+/**
+ * Load the main plugin bootstrap file.
+ */
+require_once MLL_PATH . 'includes/mozedia-lazyload.php';
 
-		//* Filter the LazyLoad HTML output
-		$html = apply_filters( 'mozedia_lazyload_html', $html, true );
+/**
+ * Load plugin translations.
+ */
+add_action( 'plugins_loaded', 'mll_load_textdomain' );
 
-		return $html;
-	} else {
-		return $matches[0];
-	}
+/**
+ * Initialize the plugin text domain.
+ */
+function mll_load_textdomain() {
+	load_plugin_textdomain( 'lazy-load-for-images', false, dirname( MLL_BASENAME ) . '/languages' );
 }
 
-// Add lazy load script in footer inline HTML
-add_action( 'wp_footer', 'mozedia_lazyload_script', PHP_INT_MAX );
-function mozedia_lazyload_script() {
-	if ( ! apply_filters( 'do_not_lazyload', true ) ) {
-		return;
-	}
-
-	echo '<script type="text/javascript">(function(a,e){function f(){var d=0;if(e.body&&e.body.offsetWidth){d=e.body.offsetHeight}if(e.compatMode=="CSS1Compat"&&e.documentElement&&e.documentElement.offsetWidth){d=e.documentElement.offsetHeight}if(a.innerWidth&&a.innerHeight){d=a.innerHeight}return d}function b(g){var d=ot=0;if(g.offsetParent){do{d+=g.offsetLeft;ot+=g.offsetTop}while(g=g.offsetParent)}return{left:d,top:ot}}function c(){var l=e.querySelectorAll("[data-src]");var j=a.pageYOffset||e.documentElement.scrollTop||e.body.scrollTop;var d=f();for(var k=0;k<l.length;k++){var h=l[k];var g=b(h).top;if(g<(d+j)){h.src=h.getAttribute("data-src");h.removeAttribute("data-src")}}}if(a.addEventListener){a.addEventListener("DOMContentLoaded",c,false);a.addEventListener("scroll",c,false)}else{a.attachEvent("onload",c);a.attachEvent("onscroll",c)}})(window,document);</script>';
-}
+/**
+ * Initialize the Smart LazyLoad plugin.
+ */
+MozediaLazyLoad\Plugin::instance();
